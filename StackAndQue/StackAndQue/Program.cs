a98479e02eb1f350b0358fe6a8e6a11bb2eb35df
VikasBenki <vikasmsbenki@gmail.com>
2022-02-03 20:52:41 +0530

    [VikasBenki] Adding the peek and pop method

--- StackAndQue/StackAndQue/Program.cs
+++ StackAndQue/StackAndQue/Program.cs
@@ -13,48 +13,48 @@ namespace StackAndQue
         static void Main(string[] args)
         {
             Console.WriteLine("Hello welcome to Stack And Que problem");
-            /// <summary>
-            /// Defines the entry point of the application.
-            /// </summary>
-            /// <param name="args">The arguments.</param>
-
+            Console.WriteLine("Welcome to Stack and Queue program using Linked List");
             CallStackQueue();
-            
+            Console.ReadLine();
         }
-            /// <summary>
-            /// Calls the stack queue.
-            /// </summary>
-            public static void CallStackQueue()
+        /// <summary>
+        /// Calls the stack queue.
+        /// </summary>
+        public static void CallStackQueue()
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            bool CONTINUE = true;
+            while (CONTINUE)
             {
-                LinkedListStack linkedListStack = new LinkedListStack();
-                bool CONTINUE = true;
-                while (CONTINUE)
+                Console.WriteLine("\nEnter your option!");
+                Console.WriteLine("1=Stack , 2=Exit");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
                 {
-                    Console.WriteLine("\nEnter your option!");
-                    Console.WriteLine("1=Stack , 2=Exit");
-                    int choice = int.Parse(Console.ReadLine());
-                    switch (choice)
-                    {
-                        case 1:
-                            Console.WriteLine("<---Creating A Stack--->");
-                            linkedListStack.Push(70);
-                            linkedListStack.Push(30);
-                            linkedListStack.Push(56);
-                            linkedListStack.Display();
-                            break;
-                        case 2:
-                            CONTINUE = false;
-                            break;
-                        default:
-                            Console.WriteLine("Enter correct option!");
-                            break;
-                    }
+                    case 1:
+                        Console.WriteLine("<---Creating A Stack---> \n");
+                        linkedListStack.Push(70);
+                        linkedListStack.Push(30);
+                        linkedListStack.Push(56);
+                        linkedListStack.Display();
+                        Console.WriteLine("\n<---PeekOperationInStack--->\n");
+                        linkedListStack.Peek();
+                        linkedListStack.Display();
+                        Console.WriteLine("\n<---PopOperation--->\n");
+                        linkedListStack.Pop();
+                        linkedListStack.Display();
+                        Console.WriteLine("\n<---PopTillStackBecomesEmpty--->\n");
+                        linkedListStack.IsEmpty();
+                        linkedListStack.Display();
+                        break;
+                    case 2:
+                        CONTINUE = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct option!");
+                        break;
                 }
-                Console.ReadLine();
             }
-        
-
-       
-        
+        }
     }
 }
