62e752409460128f8ff0be538a7be1f80e6b8e7f
VikasBenki <vikasmsbenki@gmail.com>
2022-02-03 19:50:27 +0530

    [Vikasbenki] Adding the welcome message for Stack and que problem

---
+++ StackAndQue/StackAndQue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to Stack And Que problem");
+            Console.ReadLine();
+        }
+    }
+}
