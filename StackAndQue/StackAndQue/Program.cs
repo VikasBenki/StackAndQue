420f870f3b6e4f15d670a4b09398b0ccb0fe7440
VikasBenki <vikasmsbenki@gmail.com>
2022-02-03 20:35:45 +0530

    [VikasBenki] Adding the Create Stack

--- StackAndQue/StackAndQue/Program.cs
+++ StackAndQue/StackAndQue/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using StackAndQue.Stack;
+using StackAndQue.LinkedList;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,48 @@ namespace StackAndQue
         static void Main(string[] args)
         {
             Console.WriteLine("Hello welcome to Stack And Que problem");
-            Console.ReadLine();
+            /// <summary>
+            /// Defines the entry point of the application.
+            /// </summary>
+            /// <param name="args">The arguments.</param>
+
+            CallStackQueue();
+            
         }
+            /// <summary>
+            /// Calls the stack queue.
+            /// </summary>
+            public static void CallStackQueue()
+            {
+                LinkedListStack linkedListStack = new LinkedListStack();
+                bool CONTINUE = true;
+                while (CONTINUE)
+                {
+                    Console.WriteLine("\nEnter your option!");
+                    Console.WriteLine("1=Stack , 2=Exit");
+                    int choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("<---Creating A Stack--->");
+                            linkedListStack.Push(70);
+                            linkedListStack.Push(30);
+                            linkedListStack.Push(56);
+                            linkedListStack.Display();
+                            break;
+                        case 2:
+                            CONTINUE = false;
+                            break;
+                        default:
+                            Console.WriteLine("Enter correct option!");
+                            break;
+                    }
+                }
+                Console.ReadLine();
+            }
+        
+
+       
+        
     }
 }
